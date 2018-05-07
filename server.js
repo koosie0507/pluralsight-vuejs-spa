4772f4c2cfc0310d29356f7d981e208811e71705
Andrei Olar <A.Olar@netmatch.ro>
2018-05-07 17:07:30 +0300

    Serve index.html

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const path = require("path");
 
-app.get("*", (req, res) => {
-  res.write("hey there!");
+const indexHTML = (function readIndexHtmlFile() {
+  return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
+})();
+
+app.get('*', function (req, res) {
+  res.write(indexHTML);
+  res.end();
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, function () {
   console.log(`server started at http://localhost:${port}`);
 });
