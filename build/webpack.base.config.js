420dc4b7fdd6b940ba24e6857bafe5b1a53bf71d
Andrei Olar <A.Olar@netmatch.ro>
2018-05-11 10:43:38 +0300

    SCSS and Vue

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -8,12 +8,13 @@ const config = {
   module: {
     rules: [
       { test: /(\.js$)|(\.vue$)/, loader: 'eslint-loader', exclude: /node_modules/, enforce: 'pre' },
-      { test: /(\.vue$)/,
-        loader: 'vue-loader',
-        options: {
-          css: 'css-loader',
-          'scss': 'css-loader|sass-loader'
-        }
+      { test: /(\.vue$)/, loader: 'vue-loader' },
+      { test: /(\.scss$)/,
+        use: [
+          'vue-style-loader',
+          { loader: 'css-loader', options: {modules: true} },
+          'sass-loader'
+        ]
       },
       { test: /(\.js$)/, loader: 'babel-loader', exclude: /node_modules/ },
     ]
@@ -21,7 +22,7 @@ const config = {
   resolve: {
   },
   output: {
-    path: path.resolve(__dirname,'../dist'),
+    path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
     filename: 'assets/js/[name].js'
   },
