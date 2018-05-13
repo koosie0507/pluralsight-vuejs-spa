61cd5f9da218cc3cb39f613690be85e86fa78127
Andrei Olar <andrei.olar@gmail.com>
2018-05-13 11:57:40 +0300

    Custom post property

--- src/theme/Category.vue
+++ src/theme/Category.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="columns">
     <div class="column is-one-third" v-for="(post) in posts" v-bind:key="post.id">
-      <div class="card">
-        <div class="card-content">
-          <h3>{{ post.title }}</h3>
-          {{ post.content }}
-        </div>
-        <footer class="card-footer">
-          <a class="card-footer-item" :href="post.link" target="_blank">Read More</a>
-        </footer>
-      </div>
+      <app-post :post="post"></app-post>
     </div>
   </div>
 </template>
 <script>
+import Post from './Post.vue'
+
 export default {
+  components: {
+    'app-post': Post
+  },
   data () {
     return {
       posts: [
