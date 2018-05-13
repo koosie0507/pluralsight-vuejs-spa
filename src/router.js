bae3481dd345b0ce39cad33417ccaa425df4a8ba
Andrei Olar <andrei.olar@samlex.ro>
2018-05-13 14:25:50 +0300

    Routing through JSON

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ const router = new VueRouter({
   scrollBehavior: (to, from, savedPosition) => ({y: 0}),
   routes: [
     { path: '/login', component: Login },
-    { path: '/category/:id', component: Category },
+    { path: '/category/:id', name: 'category', component: Category },
     { path: '/', redirect: '/category/front-end' }
   ]
 })
--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -3,8 +3,8 @@
     <div class="container tabs">
       <ul>
         <li><router-link to="/category/front-end"><img src="http://bit.ly/vue-img" alt="Vue SPA" /></router-link></li>
-        <li><router-link class="navbar-item is-tab" to="/category/front-end">Front-end</router-link></li>
-        <li><router-link class="navbar-item is-tab" to="/category/mobile">Mobile</router-link></li>
+        <li><router-link class="navbar-item is-tab" :to="{ name: 'category', params: { 'id': 'front-end' } }">Front-end</router-link></li>
+        <li><router-link class="navbar-item is-tab" :to="{ name: 'category', params: { 'id': 'mobile' } }">Mobile</router-link></li>
         <li><router-link class="navbar-item is-tab" to="/login">Login</router-link></li>
       </ul>
     </div>
