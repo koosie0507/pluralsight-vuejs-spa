2a65324cc0fa4d2ab1c6442a483de33550417ef6
Andrei Olar <andrei.olar@samlex.ro>
2018-05-13 14:37:28 +0300

    Add NotFound component

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Category from './theme/Category.vue'
 import Login from './theme/Login.vue'
+import NotFound from './theme/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -12,7 +13,8 @@ const router = new VueRouter({
   routes: [
     { path: '/login', component: Login },
     { path: '/category/:id', name: 'category', component: Category },
-    { path: '/', redirect: '/category/front-end' }
+    { path: '/', redirect: '/category/front-end' },
+    { path: '*', component: NotFound }
   ]
 })
 
---
+++ src/theme/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 class="is-capitalized">Not found</h2>
+    <p>You're trying to get a component that doesn't exist</p>
+    <table class="is-bordered">
+      <thead>
+      <tr>
+        <th>Property</th>
+        <th>Value</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>Path</td>
+        <td>{{this.$route.path}}</td>
+      </tr>
+      <tr>
+        <td>Query</td>
+        <td>{{this.$route.query}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
