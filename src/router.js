efc4bef9ea87e70da1038ebbe98946c6d23dbfdc
Andrei Olar <andrei.olar@samlex.ro>
2018-05-13 13:51:20 +0300

    Move category component to sub-route

--- src/router.js
+++ src/router.js
@@ -7,9 +7,12 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
+  linkActiveClass: 'is-active',
+  scrollBehavior: (to, from, savedPosition) => ({y: 0}),
   routes: [
     { path: '/login', component: Login },
-    { path: '/', component: Category }
+    { path: '/category/front-end', component: Category },
+    { path: '/', redirect: '/category/front-end' }
   ]
 })
 
--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <nav class="nav has-shadow">
     <div class="container">
-      <a href="/"><img src="http://bit.ly/vue-img" alt="Vue SPA" /></a>
+      <router-link to="/category/front-end"><img src="http://bit.ly/vue-img" alt="Vue SPA" /></router-link>
+      <router-link class="nav-item is-tab" to="/category/front-end">Home</router-link>
+      <router-link class="nav-item is-tab" to="/login">Login</router-link>
     </div>
   </nav>
 </template>
