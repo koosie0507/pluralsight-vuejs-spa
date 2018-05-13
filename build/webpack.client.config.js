1bbdb383bb72e3aff0d205ceaba8ff1041aa6b88
Andrei Olar <andrei.olar@gmail.com>
2018-05-13 12:36:44 +0300

    Extract CSS to a css file using mini-extract-css-plugin

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,7 +1,17 @@
-const base = require("./webpack.base.config");
+const base = require('./webpack.base.config')
+const MiniCssExtract = require('mini-css-extract-plugin')
 
 const config = Object.assign({}, base, {
   plugins: base.plugins || []
-});
+})
 
-module.exports = config;
+config.plugins.push(new MiniCssExtract({
+  filename: 'assets/css/[name].css',
+  chunkFilename: 'assets/css/[id].css'
+}))
+
+config.module.rules
+  .filter(r => { return r.use && r.use[0] == 'vue-style-loader' })
+  .forEach(r => { r.use.splice(0, 1, MiniCssExtract.loader) })
+
+module.exports = config
