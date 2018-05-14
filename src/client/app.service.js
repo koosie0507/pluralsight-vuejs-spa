7480f1d87b45b5d0f868d15c1cbc5d01a026ef11
Andrei Olar <A.Olar@netmatch.ro>
2018-05-14 17:46:43 +0300

    call login

--- src/client/app.service.js
+++ src/client/app.service.js
@@ -6,6 +6,17 @@ const appService = {
   getPosts (categoryId) {
     return axios.get(`/posts?category=${categoryId}`)
       .then(res => res.data)
+  },
+  login (credentials) {
+    return new Promise((resolve, reject) => {
+      axios.post('/auth/login', credentials)
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(response => {
+          reject(response.status)
+        })
+    })
   }
 }
 
--- src/client/theme/Login.vue
+++ src/client/theme/Login.vue
@@ -8,7 +8,7 @@
       <div class="field-body">
         <div class="field">
           <div class="control">
-            <input class="input" type="text"
+            <input class="input" type="text" v-model="name"
                    placeholder="Your username">
           </div>
         </div>
@@ -21,7 +21,7 @@
       <div class="field-body">
         <div class="field">
           <div class="control">
-            <input class="input" type="password"
+            <input class="input" type="password" v-model="password"
                    placeholder="Your password">
           </div>
         </div>
@@ -34,7 +34,7 @@
       <div class="field-body">
         <div class="field">
           <div class="control">
-            <button class="button is-primary">
+            <button v-on:click="login()" class="button is-primary">
               <span class="icon is-small">
                 <i class="fas fa-italic" />
               </span>
@@ -46,3 +46,24 @@
     </div>
   </div>
 </template>
+<script>
+import appService from '../app.service'
+export default {
+  data () {
+    return {
+      name: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      appService.login({name: this.name, password: this.password})
+        .then(data => {
+          window.localStorage.setItem('token', data.token)
+          window.localStorage.setItem('tokenExpiration', data.expiration)
+        })
+        .catch(() => window.alert('login failed'))
+    }
+  }
+}
+</script>
