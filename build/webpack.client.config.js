d17f79f6fd7208fba4d4ca469576191d2a618002
Andrei Olar <A.Olar@netmatch.ro>
2018-05-15 16:15:12 +0300

    Add uglify

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,7 +1,16 @@
 const base = require('./webpack.base.config')
+const webpack = require ('webpack')
 
 const config = Object.assign({}, base, {
   plugins: base.plugins || []
 })
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  )
+}
 
 module.exports = config
