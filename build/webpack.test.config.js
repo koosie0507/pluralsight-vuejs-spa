d1f03cc64685013ed3725ef8f3dcfc9e580e1947
Andrei Olar <A.Olar@netmatch.ro>
2018-05-15 14:14:58 +0300

    Make tests eslint-friendly

--- build/webpack.test.config.js
+++ build/webpack.test.config.js
@@ -3,7 +3,7 @@ const base = require('./webpack.base.config')
 let config = Object.assign({}, base, {
   mode: 'development'
 })
-config.module.rules.splice(0, 1)
+
 delete config.entry
 
 module.exports = config
