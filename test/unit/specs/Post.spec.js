79fd4c8f371942d5a8c95bac75888f9118677bf5
Andrei Olar <A.Olar@netmatch.ro>
2018-05-15 14:20:16 +0300

    Added async test

--- test/unit/specs/Post.spec.js
+++ test/unit/specs/Post.spec.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Post from '../../../src/app/theme/Post.vue'
 
+function createComponent () {
+  const PostConstructor = Vue.extend(Post)
+  const comp = new PostConstructor({
+    propsData: {
+      link: 'http://www.netmatch.nl'
+    }
+  }).$mount()
+  return comp
+}
+
 describe('Post.vue', () => {
   it('should render the link', () => {
-    const PostConstructor = Vue.extend(Post)
-    const comp = new PostConstructor({
-      propsData: {
-        link: 'http://www.netmatch.nl'
-      }
-    }).$mount()
+    const comp = createComponent()
     expect(comp.$el.querySelector('.card-footer-item').getAttribute('href'))
       .to.equal('http://www.netmatch.nl')
+  })
+
+  it('should update element\'s href when property link changes', (done) => {
+    const comp = createComponent()
+
+    comp.link = 'http://stackoverflow.com'
+
+    Vue.nextTick(() => {
+      expect(comp.$el.querySelector('.card-footer-item').getAttribute('href'))
+        .to.equal('http://stackoverflow.com')
+      done()
+    })
   })
 })
