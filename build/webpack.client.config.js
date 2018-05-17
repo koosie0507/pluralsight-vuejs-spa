ecfcf0e384b6a7f21a0d981089b0e871d00ff764
Andrei Olar <A.Olar@netmatch.ro>
2018-05-17 14:10:02 +0300

    Typo

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,5 +1,5 @@
 const base = require('./webpack.base.config')
-const webpack = require ('webpack')
+const webpack = require('webpack')
 
 const config = Object.assign({}, base, {
   plugins: base.plugins || []
