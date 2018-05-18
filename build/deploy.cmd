b6dcb49f68e79a0fc1523f68f427de1a6dc234b2
Andrei Olar <andrei.olar@samlex.ro>
2018-05-18 10:45:46 +0300

    Copy the server-side code over to the deployment target

--- build/deploy.cmd
+++ build/deploy.cmd
@@ -6,6 +6,7 @@ call cd %DEPLOYMENT_TARGET%
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
 xcopy /d %DEPLOYMENT_SOURCE%\dist\* %DEPLOYMENT_TARGET%\dist /s /i
+xcopy /d %DEPLOYMENT_SOURCE%\src\api\* %DEPLOYMENT_TARGET%\src\api /s /i
 xcopy /d %DEPLOYMENT_SOURCE%\index.html %DEPLOYMENT_TARGET%\index.html*
 xcopy /d %DEPLOYMENT_SOURCE%\server.js %DEPLOYMENT_TARGET%\server.js*
 xcopy /d %DEPLOYMENT_SOURCE%\package.json %DEPLOYMENT_TARGET%\package.json*
